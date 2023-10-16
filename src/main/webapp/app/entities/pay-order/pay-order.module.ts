6d62c9f05ea46cdc0c8bdb4e9799a67fadb3958f
azmain <azmainnishan@gmail.com>
2023-10-17 02:42:25 +0600

    installed primeng

--- src/main/webapp/app/entities/pay-order/pay-order.module.ts
+++ src/main/webapp/app/entities/pay-order/pay-order.module.ts
@@ -5,9 +5,11 @@ import { PayOrderDetailComponent } from './detail/pay-order-detail.component';
 import { PayOrderUpdateComponent } from './update/pay-order-update.component';
 import { PayOrderDeleteDialogComponent } from './delete/pay-order-delete-dialog.component';
 import { PayOrderRoutingModule } from './route/pay-order-routing.module';
+import { DropdownModule } from 'primeng/dropdown';
+import { CalendarModule } from 'primeng/calendar';
 
 @NgModule({
-  imports: [SharedModule, PayOrderRoutingModule],
+  imports: [SharedModule, PayOrderRoutingModule, DropdownModule, CalendarModule],
   declarations: [PayOrderComponent, PayOrderDetailComponent, PayOrderUpdateComponent, PayOrderDeleteDialogComponent],
 })
 export class PayOrderModule {}
